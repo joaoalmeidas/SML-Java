e9ee31fce0d79778f4be23cfbda6bab582126962
joao <jg.almeidast@gmail.com>
2018-05-31 17:23:23 -0300

    Removidos os metodos de separação da operacao e posicao dos comandos provenientes da memoria, sendo eles substituidos por códigos mais simples mas com a mesma funcionalidade. Adicionados as condicos para cada operacao presente na linguagem SML

--- src/SML.java
+++ src/SML.java
@@ -14,7 +14,7 @@ public class SML {
 		final int MULTIPLY = 33;
 		final int BRANCH = 40;
 		final int BRANCHNEG = 41;
-		final int BRACHZERO = 42;
+		final int BRANCHZERO = 42;
 		final int HALT = 43;
 		
 		String instrucao = new String();
@@ -38,30 +38,39 @@ public class SML {
 		//percorre a memoria executando os comandos
 		for(int i = 0; i < memoria.length || memoria[i] == 4300; i++) {
 			
+			int operacao, indice;
 			
+			//separa a operacao e a posicao contidos na posicao da memoria
+			operacao = memoria[i]/100;
+			indice = memoria[i]%100;
 			
+			if(operacao == READ) {
+				
+			}else if(operacao == WRITE){
+				
+			}else if(operacao == LOAD){
+				
+			}else if(operacao == STORE){
+				
+			}else if(operacao == ADD) {
+				
+			}else if(operacao == SUBTRACT) {
+				
+			}else if(operacao == DIVIDE) {
+				
+			}else if(operacao == MULTIPLY) {
+				
+			}else if(operacao == BRANCH) {
+				
+			}else if(operacao == BRANCHNEG) {
+				
+			}else if(operacao == BRANCHZERO) {
+				
+			}else if(operacao == HALT) {
+				
+			}
 		}
 		
 	}
-	
-	//separa a operacao que sera realizada
-	final int separaOperacao(int comando) {
-		int operacao;
-		
-		operacao = comando/100;
-		
-		return operacao;
-	}
-	
-	//separa a posicao da memoria que sera usada
-	final int separaPosicao(int comando) {
-		int posicao;
-		
-		posicao = comando%100;
-		
-		return posicao;
-	}
-	
-	
 
 }
