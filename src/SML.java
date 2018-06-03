021488548d3024cc3275fe388f98ab1fb07c8edb
joao <jg.almeidast@gmail.com>
2018-06-02 23:07:37 -0300

    Adicionadas as condicoes de insercao de linhas de codigo nas criacoes de programas no SML.

--- src/SML.java
+++ src/SML.java
@@ -21,97 +21,75 @@ public class SML {
 		Scanner input = new Scanner(System.in);
 		
 		int[] memoria = new int[100];
-		int posicao = 0, acumulador = 0;
+		int posicao = 0, acumulador = 0, operacao = 0, indice = 0;
 		boolean halt = false;
 		
 		System.out.println("SIMPLETRON MACHINE LANGUAGE\n\nDigite o seu código abaixo:\nc + ENTER = EXECUTAR");
 
 		//inputa os valores para a memoria
-		while(!instrucao.equals("c")) {
+		while(!instrucao.equals("c") && !instrucao.equals("C")) {
 			
 			instrucao = input.nextLine();
+			if(!instrucao.equals("c") && !instrucao.equals("C")) {
+				operacao = Integer.parseInt(instrucao)/100;
+				indice = Integer.parseInt(instrucao)/100;
+			}
 			
-			if(!(instrucao.equals("c"))){
-				memoria[posicao] = Integer.parseInt(instrucao);
-				posicao++;
+			if(!instrucao.equals("c") && !instrucao.equals("C")){
+			
+				if(indice >=0 && indice < 100 && instrucao.length() == 4 && operacao == READ || operacao == WRITE || operacao == LOAD || operacao == STORE || operacao == ADD || operacao == SUBTRACT || 
+						operacao == DIVIDE || operacao == MULTIPLY || operacao == BRANCH || operacao == BRANCHNEG || operacao == BRANCHZERO || operacao == HALT) {
+					
+						memoria[posicao] = Integer.parseInt(instrucao);
+						posicao++;
+				}else{
+					System.out.println("Comando incorreto, insira a linha de código novamente.");	
+				}
 			}
 		}
 		
 		//percorre a memoria executando os comandos
-		for(int i = 0; halt == false; i++) {
-			
-			int operacao, indice;
+		for(int i = 0; i < 100 && halt == false; i++) {
 			
 			//separa a operacao e a posicao contidos na posicao da memoria
 			operacao = memoria[i]/100;
 			indice = memoria[i]%100;
 			
 			if(operacao == READ) {
-				
 				System.out.printf("Insira um valor:");
 				memoria[indice] = input.nextInt();
-				
 			}else if(operacao == WRITE){
 				System.out.println(" = " +memoria[indice]);
-				
 			}else if(operacao == LOAD){
-				
 				System.out.printf("%d", memoria[indice]);
 				acumulador = memoria[indice];
-				
 			}else if(operacao == STORE){
-				
 				memoria[indice] = acumulador;
-				
 			}else if(operacao == ADD) {
-				
 				System.out.printf(" + " +memoria[indice]);
 				acumulador += memoria[indice];
-				
 			}else if(operacao == SUBTRACT) {
-				
 				System.out.printf(" - " +memoria[indice]);
 				acumulador -= memoria[indice];
-				
 			}else if(operacao == DIVIDE) {
-				
-				
 				System.out.printf(" / " +memoria[indice]);
 				acumulador /= memoria[indice];
-				
 			}else if(operacao == MULTIPLY) {
-				
 				System.out.printf(" * " +memoria[indice]);
 				acumulador *= memoria[indice];
-				
 			}else if(operacao == BRANCH) {
-				
 				i = indice;
-				
 			}else if(operacao == BRANCHNEG) {
-				
 				if(acumulador < 0) {
 					i = indice;
 				}
-				
 			}else if(operacao == BRANCHZERO) {
-				
 				if(acumulador == 0) {
 					i = indice;
 				}
-				
 			}else if(operacao == HALT) {
-				
-				halt = true;
-				
-			}
-			
-			
-		}
-		
-		
-		
+				halt = true;	
+			}	
+		}	
 	}
-	
-
 }
