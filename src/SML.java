93cd7b4ff5111bb9814590655baa79cf36f33171
joao <joao182cx@gmail.com>
2018-12-17 13:54:39 -0200

    Consertada a funcionalidade das 1000 posições da memória, adição da operação CONTINUE, que permite executar mais de um programa a cada execução do SML e adição de exemplos de códigos SML para as duas novas funcionalidades.

--- src/SML.java
+++ src/SML.java
@@ -17,11 +17,12 @@ public class SML {
 		final int BRANCHNEG = 41;
 		final int BRANCHZERO = 42;
 		final int HALT = 43;
+		final int CONTINUE = 44;
 		
 		int instrucao = 0;
 		Scanner input = new Scanner(System.in);
-		DecimalFormat df = new DecimalFormat("0000");
-		DecimalFormat dfMemoria = new DecimalFormat("+0000");
+		DecimalFormat df = new DecimalFormat("00000");
+		DecimalFormat dfMemoria = new DecimalFormat("+00000");
 		
 		int[] memoria = new int[1000];
 		int posicao = 0, acumulador = 0, operacao = 0, indice = 0;
@@ -30,32 +31,32 @@ public class SML {
 		System.out.println("SIMPLETRON MACHINE LANGUAGE\n"
 				+ "Bem-vindo ao Simpleton Machine Language!\n"
 				+ "Por favor, insira uma instrução por vez\n"
-				+ "\nDigite o seu código abaixo:\n-99999 + ENTER = EXECUTAR\n");
+				+ "\nDigite o seu código abaixo:\n-999999 + ENTER = EXECUTAR\n");
 
 		//inputa os valores para a memoria
 		
 		int posicaoMemoria = 0;
 		
-		while(instrucao != -99999) {
+		while(instrucao != -999999) {
 			
 			System.out.printf(df.format(posicaoMemoria) + " ? ");
 			
 			//insere linha de codigo
 			instrucao = input.nextInt();
 			
-			operacao = instrucao/100;
-			indice = instrucao%100;
+			operacao = instrucao/1000;
+			indice = instrucao%1000;
 			
 			//valida a operacao e o indice
-			if(indice >= 0 && indice < 100 && operacao == READ || operacao == WRITE || operacao == LOAD || operacao == STORE || operacao == ADD || operacao == SUBTRACT || 
+			if(indice >= 0 && indice < 1000 && operacao == READ || operacao == WRITE || operacao == LOAD || operacao == STORE || operacao == ADD || operacao == SUBTRACT || 
 			operacao == DIVIDE || operacao == MULTIPLY || operacao == BRANCH || operacao == BRANCHNEG || operacao == BRANCHZERO || 
-			operacao == HALT) {
+			operacao == HALT || operacao == CONTINUE) {
 				
 				memoria[posicao] = instrucao;
 				posicao++;
 				posicaoMemoria++;
 				
-			}else if(instrucao != -99999){
+			}else if(instrucao != -999999){
 				System.out.println("Indice ou operação inválida");
 			}
 		}
@@ -71,8 +72,8 @@ public class SML {
 			
 			int registroInstrucao = memoria[i];
 			
-			operacao = registroInstrucao/100;
-			indice = registroInstrucao%100;
+			operacao = registroInstrucao/1000;
+			indice = registroInstrucao%1000;
 			
 			if(operacao == READ) {
 				System.out.printf("Insira um valor:");
@@ -80,7 +81,7 @@ public class SML {
 			}else if(operacao == WRITE){
 				System.out.println(memoria[indice]);
 			}else if(operacao == LOAD){
-				if(acumulador < -9999 || acumulador > +9999){
+				if(acumulador < -99999 || acumulador > +99999){
 					System.out.println("Estouro de memória!\nExecução do programa finalizada.");
 					halt=true;
 				}else {
@@ -114,33 +115,48 @@ public class SML {
 				}
 			}else if(operacao == HALT) {
 				halt = true;
+			}else if(operacao == CONTINUE) {
+				
+				System.out.println("Fim da execução do programa SML!");
+				
+				exibeDump(acumulador, posicaoMemoria, registroInstrucao, operacao, indice, memoria, dfMemoria);
+				acumulador = 0;
+				
+				i = indice - 1;
+				
+				System.out.println("\nInicio da execução do novo programa...");
+				
 			}
 			
-			
+			//se halt, eximbe dump
 			if(halt == true) {
-				System.out.println("Execução do programa finalizada!!");
-				System.out.println("REGISTRADORES");
-				System.out.printf("Acumulador:\t%d\n", acumulador);
-				System.out.printf("Contador de instruções:\t%d\n", posicaoMemoria);
-				System.out.printf("Instrução atual:\t%d\n", registroInstrucao);
-				System.out.printf("Operação:\t%d\n", operacao);
-				System.out.printf("Indice:\t%d\n", indice);
-				
-				System.out.println("MEMÓRIA");
-				
-				System.out.printf("\t0\t1\t2\t3\t4\t5\t6\t7\t8\t9");
-				
-				for(int j = 0; j < memoria.length; j++){
-					if(j%10 == 0) {
-						System.out.println();
-						System.out.printf("%dx\t",j/10);
-					}
-					System.out.printf(dfMemoria.format(memoria[j]) +"\t");
-				}
+				exibeDump(acumulador, posicaoMemoria, registroInstrucao, operacao, indice, memoria, dfMemoria);
 			}
 		}
 		
 		
+	}
+	
+	public static void exibeDump(int acumulador, int posicaoMemoria, int registroInstrucao, int operacao, int indice, int[] memoria, DecimalFormat dfMemoria){
+		System.out.println("Execução do programa finalizada!!");
+		System.out.println("REGISTRADORES");
+		System.out.printf("Acumulador:\t%d\n", acumulador);
+		System.out.printf("Contador de instruções:\t%d\n", posicaoMemoria);
+		System.out.printf("Instrução atual:\t%d\n", registroInstrucao);
+		System.out.printf("Operação:\t%d\n", operacao);
+		System.out.printf("Indice:\t%d\n", indice);
+		
+		System.out.println("MEMÓRIA");
+		
+		System.out.printf("\t0\t1\t2\t3\t4\t5\t6\t7\t8\t9");
+		
+		for(int j = 0; j < memoria.length; j++){
+			if(j%10 == 0) {
+				System.out.println();
+				System.out.printf("%dx\t",j/10);
+			}
+			System.out.printf(dfMemoria.format(memoria[j]) +"\t");
+		}
 	}
 	
 }
